a4e2eccaefc1bf78c01dbd11d6e87f34fca04ba5
Maria Rosenova <93831078+MariaRosenova@users.noreply.github.com>
2024-03-24 20:25:37 +0200

    Add new paths

--- caliestenics-app/src/app/app-routing.module.ts
+++ caliestenics-app/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
+import { HomeComponent } from './home/home/home.component';
 
 const routes: Routes = [
   {
-    path: 'login', component: LoginComponent
+    path: '', pathMatch: 'full', redirectTo: '/home'
+  },
+  {
+    path: 'home', component: HomeComponent
   },
   {
     path: 'register', component: RegisterComponent
