d95fbe940569daf6efba435d046756e55ecf7f64
Maria Rosenova <93831078+MariaRosenova@users.noreply.github.com>
2024-03-25 11:20:48 +0200

    Create a button component

--- caliestenics-app/src/app/shared/shared.module.ts
+++ caliestenics-app/src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainViewComponent } from './main-view/main-view.component';
 import { SecondViewComponent } from './second-view/second-view.component';
+import { ButtonComponent } from './button/button.component';
 
 
 
 @NgModule({
   declarations: [
     MainViewComponent,
-    SecondViewComponent
+    SecondViewComponent,
+    ButtonComponent
   ],
   imports: [
     CommonModule,
