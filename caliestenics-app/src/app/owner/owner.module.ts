ecb1bbe5b8ceb54462abf50e8d2cd9220d17b3ba
Maria Rosenova <93831078+MariaRosenova@users.noreply.github.com>
2024-03-29 20:44:07 +0200

    Create owner module
    Also create a component for creating exercises

---
+++ caliestenics-app/src/app/owner/owner.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CreateExerciseComponent } from './create-exercise/create-exercise.component';
+
+
+
+@NgModule({
+  declarations: [
+    CreateExerciseComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class OwnerModule { }
