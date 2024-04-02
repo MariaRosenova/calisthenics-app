345cee090c1c6f55954a447dc74cffd442e4a898
Maria Rosenova <93831078+MariaRosenova@users.noreply.github.com>
2024-04-02 10:30:53 +0300

    Edit training module

--- caliestenics-app/src/app/training/training.module.ts
+++ caliestenics-app/src/app/training/training.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrainingComponent } from './training/training.component';
 import { SharedModule } from '../shared/shared.module';
+import { ProgramComponent } from './program/program.component';
 
 
 
 @NgModule({
   declarations: [
-    TrainingComponent
+    TrainingComponent,
+    ProgramComponent
   ],
   imports: [
     CommonModule,
     SharedModule
   ],
   exports: [
-    TrainingComponent
+    TrainingComponent,
+    ProgramComponent
   ]
 })
 export class TrainingModule { }
