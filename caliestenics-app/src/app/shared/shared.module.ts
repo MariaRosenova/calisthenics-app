f82b7b08f0fecb2aa9244188098ecb416ac69dce
Maria Rosenova <93831078+MariaRosenova@users.noreply.github.com>
2024-04-03 14:37:26 +0300

    Remove view components

--- caliestenics-app/src/app/shared/shared.module.ts
+++ caliestenics-app/src/app/shared/shared.module.ts
@@ -1,25 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainViewComponent } from './main-view/main-view.component';
-import { SecondViewComponent } from './second-view/second-view.component';
-import { ThirdViewComponent } from './third-view/third-view.component';
-
 
 
 
 @NgModule({
   declarations: [
     MainViewComponent,
-    SecondViewComponent,
-    ThirdViewComponent,
   ],
   imports: [
     CommonModule,
   ],
   exports: [
     MainViewComponent,
-    SecondViewComponent,
-    ThirdViewComponent
   ]
 })
 export class SharedModule { }
