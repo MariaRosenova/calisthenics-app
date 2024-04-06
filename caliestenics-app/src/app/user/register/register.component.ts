fdf98bfb893b505139aead7e556da3eb1fc39b06
Maria Rosenova <93831078+MariaRosenova@users.noreply.github.com>
2024-04-06 10:45:50 +0300

    Edit register form

--- caliestenics-app/src/app/user/register/register.component.ts
+++ caliestenics-app/src/app/user/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { EMAIL_DOMAINS } from 'src/app/constants';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  domains = EMAIL_DOMAINS;
 
+  register(form: NgForm) {
+    if(form.invalid) {
+      return;
+    }
+    console.log(form.value);
+    console.log('successful registered user')
+  }
 }
