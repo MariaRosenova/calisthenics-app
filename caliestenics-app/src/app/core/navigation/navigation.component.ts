19acd8dcf5f35f95320ab90031ee7caf3370c2d6
Maria Rosenova <93831078+MariaRosenova@users.noreply.github.com>
2024-04-07 10:53:02 +0300

    Add navigation logic

--- caliestenics-app/src/app/core/navigation/navigation.component.ts
+++ caliestenics-app/src/app/core/navigation/navigation.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { faLink} from '@fortawesome/free-solid-svg-icons';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faLink, faBars} from '@fortawesome/free-solid-svg-icons';
+import { UserService } from 'src/app/user/user.service';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -8,6 +10,21 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent {
+  constructor(private userService: UserService, private router: Router) {
+  }
+
   faLink = faLink;
   faBars = faBars;
+  faUserCircle = faUserCircle;
+ 
+
+  get isLoggedIn(): boolean {
+    return this.userService.isLogged;
+  }
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['/home']);
+  }
+
 }
