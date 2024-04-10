735c3c2362f23266edbd4e70849e8b8a382c92a2
Maria Rosenova <93831078+MariaRosenova@users.noreply.github.com>
2024-04-10 18:37:17 +0300

    Edit register in order to get data

--- caliestenics-app/src/app/user/register/register.component.ts
+++ caliestenics-app/src/app/user/register/register.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, NgForm, Validators } from '@angular/forms';
 import { EMAIL_DOMAINS } from 'src/app/constants';
 import { emailValidator } from 'src/app/shared/utils/email-validator';
 import { matchPasswordsValidator } from 'src/app/shared/utils/matchPasswordsValidator';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -27,13 +29,25 @@ export class RegisterComponent {
     return this.form.get('passGroup');
   }
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private userService: UserService, private router: Router) {}
 
   register(): void {
     if (this.form.invalid) {
       return;
     }
 
-    console.log(this.form.value);
+    const {
+      email, 
+      username, 
+      passGroup: {password, rePassword} = {}} = this.form.value;
+
+    this.userService.register(
+      email!, 
+      username!, 
+      password!, 
+      rePassword!
+    ).subscribe(() => {
+      this.router.navigate(['/training']);
+    })
 }
 }
